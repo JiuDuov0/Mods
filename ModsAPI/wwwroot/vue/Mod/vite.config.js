6472b0112968d08b4e30a5bf3c163afd950ce044
朵 九 <2457843376@qq.com>
2025-03-13 19:40:29 +0800

    vue发布纯狗屎

--- ModsAPI/wwwroot/vue/Mod/vite.config.js
+++ ModsAPI/wwwroot/vue/Mod/vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/bserver': {
+        target: 'http://43.160.202.17:8099',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/bserver/, '')
+      }
+    }
+  },
 })
