7280df2d06f1e5433f21536f74fd99bde12d150a
JiuDuov0 <2457843376@qq.com>
2025-03-27 16:56:41 +0800

    重新生成

--- ModsAPI/Controllers/LoginController.cs
+++ ModsAPI/Controllers/LoginController.cs
@@ -104,10 +104,10 @@ namespace ModsAPI.Controllers
             var UserInfo = _IUserService.Login((string)json.LoginAccount, (string)json.Password);
             if (UserInfo != null && UserInfo.UserId != null)
             {
-                if (!string.IsNullOrWhiteSpace(UserInfo.Token))
-                {
-                    return new ResultEntity<ResponseToken>() { ResultData = new ResponseToken() { Token = UserInfo.Token } };
-                }
+                //if (!string.IsNullOrWhiteSpace(UserInfo.Token))
+                //{
+                //    return new ResultEntity<ResponseToken>() { ResultData = new ResponseToken() { Token = UserInfo.Token } };
+                //}
                 var res = _JwtHelper.CreateYearsToken(UserInfo);
                 UserInfo.Token = res.Token;
                 _IUserService.UpdateUserAsync(UserInfo);
